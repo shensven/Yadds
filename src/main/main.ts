a279516856a387dfb3232051552724f93b4e66a5
SvenFE <shensven2016@gmail.com>
2021-12-13 14:31:05 +0800

    feat: set the menu bar invisible for Windows and Linux

--- src/main/main.ts
+++ src/main/main.ts
@@ -80,6 +80,8 @@ const createWindow = async () => {
     },
   });
 
+  mainWindow.menuBarVisible = false;
+
   mainWindow.loadURL(resolveHtmlPath('index.html'));
 
   mainWindow.on('ready-to-show', () => {
