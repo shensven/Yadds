0cb745b9d23be743f31e9f9e197021946583fa92
SvenFE <shensven2016@gmail.com>
2022-03-12 00:50:49 +0800

    feat: fall back to the English entry if it's not hit

--- src/renderer/i18n/i18n.ts
+++ src/renderer/i18n/i18n.ts
@@ -17,6 +17,7 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
+    fallbackLng: 'en',
     lng: (window.electron?.store.get('yaddsI18nCode') as string) ?? 'en',
     // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
