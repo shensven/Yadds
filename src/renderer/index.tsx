d711f32ddda50483f705e22ea36247d0136c44e9
SvenFE <shensven2016@gmail.com>
2022-08-18 14:09:52 +0800

    style: coding refine

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const container = document.getElementById('root');
-const root = createRoot(container!);
+const container = document.getElementById('root') as HTMLElement;
+const root = createRoot(container);
 root.render(<App />);
 
 // calling IPC exposed from preload script
