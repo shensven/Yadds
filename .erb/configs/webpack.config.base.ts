1fd897ea3a969ba78c97c7a5229918667b47fb4f
SvenFE <shensven2016@gmail.com>
2022-11-11 11:49:41 +0800

    chore(webpack): path alias

--- .erb/configs/webpack.config.base.ts
+++ .erb/configs/webpack.config.base.ts
@@ -41,6 +41,9 @@ const configuration: webpack.Configuration = {
   resolve: {
     extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
     modules: [webpackPaths.srcPath, 'node_modules'],
+    alias: {
+      '@': webpackPaths.srcPath,
+    }
   },
 
   plugins: [
